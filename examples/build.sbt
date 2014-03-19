a2ea129b3c0c3097ebc44585ff515330968c656f
levkhomich <levkhomich@gmail.com>
2014-03-20 02:01:56 +0600

    add example

---
+++ examples/build.sbt
@@ -0,0 +1,13 @@
+name := "akka-tracing-examples"
+
+version := "0.1.0"
+
+scalaVersion := "2.10.3"
+
+resolvers += "Maven Central Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+
+libraryDependencies += "com.github.levkhomich.akka.tracing" %% "akka-tracing-core" % "0.1.0-SNAPSHOT" changing()
+
+libraryDependencies += "com.typesafe" % "config" % "1.0.2"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.0"
---
+++ examples/src/main/scala/org/example/TraceHierarchy.scala
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2014 the Akka Tracing contributors. See AUTHORS for more details.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example
+
+import akka.actor.{ActorSystem, Props, ActorRef, Actor}
+import akka.pattern.{ask, pipe}
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+import com.github.levkhomich.akka.tracing.{ActorTracing, TracingSupport}
+import scala.util.Random
+import java.util.UUID
+import com.typesafe.config.{ConfigFactory, Config}
+
+final case class ExternalRequest(headers: Map[String, String], payload: String) extends TracingSupport
+final case class ExternalResponse(responseCode: Int, payload: String)
+final case class InternalRequest(payload: String) extends TracingSupport
+final case class InternalResponse(responseCode: Int, payload: String)
+
+class RequestHandler extends Actor with ActorTracing {
+
+  import context.dispatcher
+
+  val child: ActorRef = context.actorOf(Props[DelegateActor])
+  implicit val askTimeout: Timeout = 200.milliseconds
+
+  override def receive: Receive = {
+    case msg @ ExternalRequest(headers, payload) =>
+      // notify tracing extension about external request to be sampled and traced
+      trace.sample(msg)
+      // name service processing request
+      trace.recordRPCName(msg, this.getClass.getSimpleName)
+
+      // add info about request headers to trace
+      headers.foreach { case (k, v) => trace.recordKeyValue(msg, k, v)}
+
+      child ? InternalRequest(payload).asChildOf(msg) recover {
+        case e: Exception =>
+          // trace exception
+          trace.record(msg, e.toString)
+          InternalResponse(500, "")
+      } map {
+        case InternalResponse(responseCode, resp) =>
+          // close trace by marking response
+          ExternalResponse(responseCode, resp + '!').asResponseTo(msg)
+      } pipeTo sender
+  }
+}
+
+class DelegateActor extends Actor with ActorTracing {
+
+  override def receive: Receive = {
+    case msg @ InternalRequest(payload) =>
+      trace.sample(msg)
+      trace.recordRPCName(msg, this.getClass.getSimpleName)
+      // another computation (sometimes leading to timeout)
+      Thread.sleep(Random.nextInt(30))
+      sender ! InternalResponse(200, s"Hello, $payload").asResponseTo(msg)
+  }
+}
+
+object TraceHierarchy extends App {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  implicit val askTimeout: Timeout = 500.milliseconds
+
+  def random = UUID.randomUUID().toString
+
+  val system = ActorSystem.create("TraceHierarchySystem", ConfigFactory.load("application"))
+  val handler = system.actorOf(Props[RequestHandler])
+
+  for (_ <- 1 to 100)
+    handler ? ExternalRequest(Map("userAgent" -> random), random)
+
+  system.awaitTermination()
+}
