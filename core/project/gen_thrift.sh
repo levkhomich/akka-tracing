7450f994499ea10cc4a1a71427953717402c5968
Lev Khomich <levkhomich@gmail.com>
2014-05-14 01:09:21 +0600

    update gen_thrift

--- core/project/gen_thrift.sh
+++ core/project/gen_thrift.sh
@@ -5,5 +5,5 @@ MANAGED_DIR=$2
 
 mkdir -p $MANAGED_DIR
 thrift -r --gen java:nocamel -out $MANAGED_DIR $THRIFT_SRC
-sed -i.bak 's/extends org.apache.thrift.async.TAsyncMethodCall/&<Log_call>/' `find $MANAGED_DIR -type f -name Scribe.java`
+sed -i.bak 's/extends org.apache.thrift.async.TAsyncMethodCall /&<Log_call> /' `find $MANAGED_DIR -type f -name Scribe.java`
 find $MANAGED_DIR -type f -name Scribe.java.bak -exec rm {} \;
