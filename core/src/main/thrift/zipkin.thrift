37905cb977ea586de7e09826f54723185e2d1b3f
Lev Khomich <levkhomich@gmail.com>
2014-07-03 20:24:43 +0600

    add new annotations to service's thrift spec

--- core/src/main/thrift/zipkin.thrift
+++ core/src/main/thrift/zipkin.thrift
@@ -1,19 +1,3 @@
-/**
- * Copyright 2014 the Akka Tracing contributors. See AUTHORS for more details.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 include "scribe.thrift"
 
 namespace java com.github.levkhomich.akka.tracing.thrift
@@ -24,6 +8,10 @@ const string SERVER_SEND = "ss"
 const string SERVER_RECV = "sr"
 const string SERVER_ADDR = "sa"
 const string CLIENT_ADDR = "ca"
+const string CLIENT_SEND_FRAGMENT = "csf"
+const string CLIENT_RECV_FRAGMENT = "crf"
+const string SERVER_SEND_FRAGMENT = "ssf"
+const string SERVER_RECV_FRAGMENT = "srf"
 
 struct Endpoint {
   1: i32 ipv4,
