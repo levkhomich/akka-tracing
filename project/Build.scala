53636738febf5aa238d0d19aef81685950e6911e
Lev Khomich <levkhomich@gmail.com>
2014-12-25 04:30:34 +0500

    bump project version

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object AkkaTracingBuild extends Build {
     publicationSettings ++
     Seq (
       organization := "com.github.levkhomich",
-      version := "0.4",
+      version := "0.5-SNAPSHOT",
       homepage := Some(url("https://github.com/levkhomich/akka-tracing")),
       startYear := Some(2014),
       licenses := Seq("Apache Public License 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0"))
