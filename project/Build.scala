b75fb667e59b49e7e5462e1ecf87ca05511bd6db
Lev Khomich <levkhomich@gmail.com>
2017-03-27 16:53:54 +0500

    bump to 0.6.1

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,7 @@ object AkkaTracingBuild extends Build {
     scalariformSettings ++
     Seq (
       organization := "com.github.levkhomich",
-      version := "0.6",
+      version := "0.6.1-SNAPSHOT",
       homepage := Some(url("https://github.com/levkhomich/akka-tracing")),
       startYear := Some(2014),
       licenses := Seq("Apache Public License 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
